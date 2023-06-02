dd5fa545ae865e01c7338a7964d705be89476160
Siddhant2002 <siddhantswarupmallick@gmail.com>
2023-06-02 18:36:09 +0530

    Pages added

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const links = document.getElementsByClassName("tab-links");
 const contents = document.getElementsByClassName("tab-contents");
 
-const opentab = (tabname) =>{
-    for(link of links)
-    {
+const opentab = (tabname) => {
+    for (link of links) {
         link.classList.remove("active-links");
     }
-    for(content of contents)
-    {
+    for (content of contents) {
         content.classList.remove("active-tab");
     }
     event.currentTarget.classList.add("active-links");
@@ -16,10 +14,27 @@ const opentab = (tabname) =>{
 
 const menu = document.getElementsByClassName("menu");
 
-open = () =>{
+open = () => {
     menu.style.right = "0";
 }
 
-close = () =>{
+close = () => {
     menu.style.right = "-200px";
 }
+
+const scriptURL = "https://script.google.com/macros/s/AKfycbxeBhyUmm1x0ERh5caJyk1p6HSwtNcT_tWoihdQWqS0CYUY3D1lKjP7_t2gKRPuAFOF/exec";
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById("msg");
+
+    form.addEventListener('submit', e => {
+        e.preventDefault()
+        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+            .then(response => {
+                msg.innerHTML = "Message sent successfully"
+                setTimeout(function(){
+                    msg.innerHTML = "";
+                },4000)
+                form.reset();
+            })
+            .catch(error => console.error('Error!', error.message))
+    })
